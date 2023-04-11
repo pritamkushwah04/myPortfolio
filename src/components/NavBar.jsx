4f15db0454164ac43b54d114f528259bfc3a077d
pritamkushwah04 <pritamkushwah.04.08@gmail.com>
2023-04-11 20:20:37 +0530

    BUG: MobileView

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -29,7 +29,7 @@ const NavBar = () => {
   ];
 
   return (
-    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+    <div className="sticky top-0 flex justify-between items-center w-full h-20 px-4 text-white bg-black x ">
       <div>
         <h1 className="text-5xl font-signature ml-2">Pritam</h1>
       </div>
