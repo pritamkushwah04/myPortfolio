08d632987f11c1223fe083f0e11cb02d5db41346
pritamkushwah04 <pritamkushwah.04.08@gmail.com>
2023-09-24 01:16:17 +0530

    footer added for small screen

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -9,7 +9,7 @@ const SocialLinks = () => {
       id: 1,
       child: (
         <>
-          LinkedIn <FaLinkedin size={30} />
+          LinkedIn<FaLinkedin size={30}  />
         </>
       ),
       href: "https://www.linkedin.com/in/pritamk47/",
@@ -19,7 +19,7 @@ const SocialLinks = () => {
       id: 2,
       child: (
         <>
-          GitHub <FaGithub size={30} />
+          GitHub<FaGithub size={30} /> 
         </>
       ),
       href: "https://github.com/pritamkushwah04",
@@ -28,7 +28,7 @@ const SocialLinks = () => {
       id: 3,
       child: (
         <>
-          Mail <HiOutlineMail size={30} />
+          Mail<HiOutlineMail size={30}  /> 
         </>
       ),
       href: "mailto:Pritamkushwah.04.08@gmail.com",
@@ -37,7 +37,7 @@ const SocialLinks = () => {
       id: 4,
       child: (
         <>
-          Resume <BsFillPersonLinesFill size={30} />
+          Resume<BsFillPersonLinesFill size={30} /> 
         </>
       ),
       href: "https://drive.google.com/file/d/1dTP55vXXjFd9SIgVOlg-RztwQQeajR6g/view",
@@ -47,30 +47,54 @@ const SocialLinks = () => {
   ];
 
   return (
-    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
-      <ul>
-        {links.map(({ id, child, href, style, download }) => (
-          <li
-            key={id}
-            className={
-              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
-              " " +
-              style
-            }
-          >
-            <a
-              href={href}
-              className="flex justify-between items-center w-full text-white"
-              download={download}
-              target="_blank"
-              rel="noreferrer"
+    <>
+      <div className="hidden sm:flex flex-col top-[35%] left-0 fixed">
+        <ul>
+          {links.map(({ id, child, href, style, download }) => (
+            <li
+              key={id}
+              className={
+                "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
+                " " +
+                style
+              }
             >
-              {child}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
+              <a
+                href={href}
+                className="flex justify-between items-center w-full text-white"
+                download={download}
+                target="_blank"
+                rel="noreferrer"
+              >
+                {child}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className=" sm:hidden bg-gradient-to-b from-gray-800 to-black">
+          {links.map(({ id, child, href, style, download }) => (
+            <div
+              key={id}
+              className={
+                "flex justify-between items-center  h-14 px-4 " +
+                " " 
+              }
+            >
+              <a
+                href={href}
+                className="flex justify-between items-center w-full text-white"
+                download={download}
+                target="_blank"
+                rel="noreferrer"
+              >
+                {child}
+              </a>
+            </div>
+          ))}
+      </div>
+    </>
+
   );
 };
 
